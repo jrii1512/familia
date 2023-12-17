ce096c019dcd9c4e70969ed82ec335ee3bfb5a24
riimalaj <jriimala@gmail.com>
2023-12-17 17:09:54 +0200

    Style improvements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,8 @@ export default function Home(){
 return (
     <div className = "home-container">
         <h1 className = "home-container-title" >Tervetuloa JRLA - kuvagalleriian</h1>
-        <span className="home-container-text">
-            Huikealla sitella on lähinä kuvia elämäni tähtihetkistä, tiikereitämme unohtamatta. Kuvien perusteella näet kiinnostukseni kohteet tässä elämässä esim moottoripyöräilyn ja chillailun ystävieni ja perheeni parissa.
+        <span>
+            Huikealla sitella on lähinä kuvia elämäni tähtihetkistä, tiikereitämme unohtamatta.<p></p>Kuvien perusteella näet kiinnostukseni kohteet tässä elämässä esim moottoripyöräilyn ja chillailun ystävieni ja perheeni parissa.
         </span>
     </div>
 )
