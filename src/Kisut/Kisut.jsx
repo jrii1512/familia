21bd5c57e18d1c6fe912db61f2d0c09aa7ffe86b
riimalaj <jriimala@gmail.com>
2023-12-18 20:02:18 +0200

    Kisut style fixes

--- src/Kisut/Kisut.jsx
+++ src/Kisut/Kisut.jsx
@@ -3,20 +3,25 @@ import kisuData from "../aLista.json";
 
 export default function Kisut() {
   const kisulit = kisuData?.map((k) => (
-      <div key={k.id} className="kisu-list-tile">
-        <Link to={`/kisut/${k.id}`}>
-          <img src={k.photo} alt={k.name} />
-        </Link>
-        <div className="kisu-list-info">
-          <h3>Info:</h3>
-          <h3>Nimi: {k.name}</h3>
-          <h3>Sukupuoli: {k.gender}</h3>
-          <h3>Tähdet: {k.stars}</h3>
-        </div>
+    <div key={k.id} className="kisu-list-tile">
+      <Link to={`/kisut/${k.id}`}>
+        <img src={k.photo} alt={k.name} />
+      </Link>
+      <div className="kisu-list-info">
+        <h3>Info:</h3>
+        <h3>Nimi: {k.name}</h3>
+        <h3>Sukupuoli: {k.gender}</h3>
+        <h3>Tähdet: {k.stars}</h3>
       </div>
+    </div>
   ));
 
   //
 
-  return <div className="kisu-list-container">{kisulit}</div>;
+  return (
+    <>
+      <div className="kisu-list-title">Klikkaa imagea</div>
+      <div className="kisu-list-container">{kisulit}</div>
+    </>
+  );
 }
